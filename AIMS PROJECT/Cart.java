1e38fcedb736de1a368cb79201d3e3aec4f3da61
Justin Nguyen <justinduycoi@gmail.com>
2026-04-14 14:51:12 +0700

    bai3

--- AIMS PROJECT/Cart.java
+++ AIMS PROJECT/Cart.java
@@ -3,18 +3,28 @@ public class Cart {
     private DigitalVideoDisc[] itemsOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private int qtyOrdered = 0;
 
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        if(qtyOrdered >= MAX_NUMBERS_ORDERED){
+            System.out.println("The cart is already full.");
+            return;
+        }
+        itemsOrdered[qtyOrdered] = disc;
+        qtyOrdered++;
+        System.out.println("The disc has been added.");
+        if(qtyOrdered == MAX_NUMBERS_ORDERED) System.out.println("The cart is full.");
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2){
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc((dvd2));
+    }
+
     public void addDigitalVideoDisc(DigitalVideoDisc[] discList) {
         for (DigitalVideoDisc disc : discList) {
-            if (qtyOrdered < MAX_NUMBERS_ORDERED) {
-                itemsOrdered[qtyOrdered] = disc;
-                qtyOrdered++;
-                System.out.println("The disc \"" + disc.getTitle() + "\" has been added.");
-            } else {
-                System.out.println("Cannot add \"" + disc.getTitle() + "\": The cart is already full.");
-                break;
-            }
+            addDigitalVideoDisc(disc);
         }
     }
+
     public void removeDigitalVideoDisc(DigitalVideoDisc disc){
         for(int i = 0; i < qtyOrdered; i++){
             if(itemsOrdered[i].equals(disc)){
---
+++ AIMS PROJECT/TestPassingParameter.java
@@ -0,0 +1,47 @@
+public class TestPassingParameter {
+    public static void main(String[] args) {
+        //TODO Auto_generated method stub
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderelle dvd title: " + cinderellaDVD.getTitle());
+
+     //   changeTitle(jungleDVD, cinderellaDVD.getTitle());
+    //System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+
+        //Ham swap moi
+        DVDWrapper w1 = new DVDWrapper(jungleDVD);
+        DVDWrapper w2 = new DVDWrapper(cinderellaDVD);
+        System.out.println("jungle dvd title: " + w1.disc.getTitle());
+        System.out.println("cinderella dvd title: " + w2.disc.getTitle());
+        swapper(w1, w2);
+        System.out.println("after swap, jungle dvd title: " + w1.disc.getTitle());
+        System.out.println("after swap, cinderella dvd title: " + w2.disc.getTitle());
+    }
+
+    public static void swap(Object o1, Object o2){
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title){
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+    static class DVDWrapper{
+        DigitalVideoDisc disc;
+        DVDWrapper(DigitalVideoDisc disc){
+            this.disc = disc;
+        }
+    }
+    public static void swapper(DVDWrapper w1, DVDWrapper w2){
+        DigitalVideoDisc tmp = w1.disc;
+        w1.disc = w2.disc;
+        w2.disc = tmp;
+    }
+}
+
