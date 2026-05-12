56a2020d99e6be6a43392f80aef5fb8df53fdf00
Justin Nguyen <justinduycoi@gmail.com>
2026-05-12 14:33:36 +0700

    5.1 Create the disc class extending the Media class

--- AIMS PROJECT/hust/soict/aims/media/DigitalVideoDisc.java
+++ AIMS PROJECT/hust/soict/aims/media/DigitalVideoDisc.java
@@ -1,9 +1,6 @@
 package hust.soict.aims.media;
 
 public class DigitalVideoDisc extends Disc {
-    private String director;
-    private int length;
-
     private static int nbDigitalVideoDiscs = 0;
 
 
