5cdc61346201332b330742b7e562631c1d6f1b21
Justin Nguyen <justinduycoi@gmail.com>
2026-05-17 21:08:04 +0700

    Add Playable interface

---
+++ AIMS PROJECT/hust/soict/aims/media/CompactDisc.java
@@ -0,0 +1,58 @@
+package hust.soict.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompactDisc extends Media implements Playable {
+    private String artist;
+    private ArrayList<Track> tracks;
+
+    public CompactDisc(int id, String title, String category, float cost, String artist){
+        super(id, title, category, cost);
+        this.artist = artist;
+        this.tracks = new ArrayList<Track>();
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void addTrack(Track track){
+        if(tracks.contains(track)){
+            System.out.println("Track da ton tai trong CD.");
+        }
+        else{
+            tracks.add(track);
+            System.out.println("Da them track vao CD.");
+        }
+    }
+
+    public void removeTrack(Track track){
+        if(tracks.contains(track)){
+            tracks.remove(track);
+            System.out.println("Da xoa track khoi CD");
+        }
+        else {
+            System.out.println("Track khong ton tai trong CD.");
+        }
+    }
+
+    public int getLength(){
+        int totalLength = 0;
+        for(Track track : tracks){
+            totalLength += track.getLength();
+        }
+        return totalLength;
+    }
+
+    @Override
+    public void play(){
+        System.out.println("Playing CD: " + this.getTitle());
+        System.out.println("CD Artist: " + this.getArtist());
+        System.out.println("Total Cd length: " + this.getLength());
+
+        for(Track track : tracks){
+            track.play();
+        }
+    }
+}
--- AIMS PROJECT/hust/soict/aims/media/DigitalVideoDisc.java
+++ AIMS PROJECT/hust/soict/aims/media/DigitalVideoDisc.java
@@ -1,6 +1,6 @@
 package hust.soict.aims.media;
 
-public class DigitalVideoDisc extends Disc {
+public class DigitalVideoDisc extends Disc implements Playable {
     private static int nbDigitalVideoDiscs = 0;
 
     public DigitalVideoDisc(String title){
@@ -23,5 +23,11 @@ public class DigitalVideoDisc extends Disc {
     public boolean isMatch(String title) {
         if (title == null) return false;
         return getTitle().equalsIgnoreCase(title);
+    }
+
+    @Override
+    public void play(){
+        System.out.println("Playing DVD: " + this.getTitle());
+        System.out.println("DVD length: " + this.getLength());
     }
 }
